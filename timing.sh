3d466a0d4f5487860c60609e6e86c16ef3aa92ad
Tim Yates <tim.yates@gmail.com>
2022-08-31 13:09:10 +0100

    Make sure retry isn't backing off

--- timing.sh
+++ timing.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 execute() {
-    curl --silent --retry 200 --retry-all-errors http://localhost:8080/hello/tim
+    curl --silent --retry 200 --retry-delay 0 --retry-all-errors http://localhost:8080/hello/tim
 }
 
 CONTAINER=$(docker run -d --rm -p 8080:8080 --privileged $1)
