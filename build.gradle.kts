1beff585f22128b7632aab73b706211b42941221
Tim Yates <tim.yates@gmail.com>
2022-09-28 15:50:25 +0100

    Use micronaut 3.7.1 snapshot and configure docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+import com.bmuschko.gradle.docker.tasks.image.DockerPushImage
 import java.net.URI
 
 plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("io.micronaut.minimal.application") version "3.6.0-SNAPSHOT"
-    id("io.micronaut.docker") version "3.6.0-SNAPSHOT"
-    id("io.micronaut.crac") version "3.6.0-SNAPSHOT"
+    id("io.micronaut.minimal.application") version "3.6.0"
+    id("io.micronaut.docker") version "3.6.0"
+    id("io.micronaut.crac") version "3.6.0"
 }
 
 version = "0.1"
@@ -24,7 +26,7 @@ dependencies {
     implementation("jakarta.annotation:jakarta.annotation-api")
     runtimeOnly("ch.qos.logback:logback-classic")
     implementation("io.micronaut:micronaut-validation")
-    implementation("io.micronaut.crac:micronaut-crac:1.0.0-SNAPSHOT")
+    implementation("io.micronaut.crac:micronaut-crac")
 }
 
 application {
@@ -34,6 +36,19 @@ application {
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.named("dockerBuildCrac", DockerBuildImage::class) {
+    images.set(listOf("timyates/hello:latest"))
+}
+
+tasks.named("dockerPushCrac", DockerPushImage::class) {
+    images.set(listOf("timyates/hello:latest"))
+    registryCredentials {
+        username.set(System.getenv("DOCKER_USERNAME"))
+        password.set(System.getenv("DOCKER_PASSWORD"))
+        email.set(System.getenv("DOCKER_EMAIL"))
     }
 }
 
